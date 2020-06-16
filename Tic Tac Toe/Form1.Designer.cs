706397f9db613861375da419dac10aca9a1346f6
Radoslaw <Radoslaw.janas@interia.eu>
2020-06-16 02:20:45 +0200

    1: App structure

---
+++ Tic Tac Toe/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Tic_Tac_Toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.K1 = new System.Windows.Forms.Button();
+            this.K2 = new System.Windows.Forms.Button();
+            this.K3 = new System.Windows.Forms.Button();
+            this.L1 = new System.Windows.Forms.Button();
+            this.L2 = new System.Windows.Forms.Button();
+            this.Z1 = new System.Windows.Forms.Button();
+            this.Z2 = new System.Windows.Forms.Button();
+            this.Z3 = new System.Windows.Forms.Button();
+            this.L3 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(402, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newGameToolStripMenuItem.Text = "New game";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // K1
+            // 
+            this.K1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.K1.Location = new System.Drawing.Point(6, 29);
+            this.K1.Name = "K1";
+            this.K1.Size = new System.Drawing.Size(125, 125);
+            this.K1.TabIndex = 1;
+            this.K1.UseVisualStyleBackColor = true;
+            // 
+            // K2
+            // 
+            this.K2.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.K2.Location = new System.Drawing.Point(137, 29);
+            this.K2.Name = "K2";
+            this.K2.Size = new System.Drawing.Size(125, 125);
+            this.K2.TabIndex = 2;
+            this.K2.UseVisualStyleBackColor = true;
+            // 
+            // K3
+            // 
+            this.K3.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.K3.Location = new System.Drawing.Point(268, 29);
+            this.K3.Name = "K3";
+            this.K3.Size = new System.Drawing.Size(125, 125);
+            this.K3.TabIndex = 3;
+            this.K3.UseVisualStyleBackColor = true;
+            // 
+            // L1
+            // 
+            this.L1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.L1.Location = new System.Drawing.Point(6, 160);
+            this.L1.Name = "L1";
+            this.L1.Size = new System.Drawing.Size(125, 125);
+            this.L1.TabIndex = 4;
+            this.L1.UseVisualStyleBackColor = true;
+            // 
+            // L2
+            // 
+            this.L2.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.L2.Location = new System.Drawing.Point(137, 160);
+            this.L2.Name = "L2";
+            this.L2.Size = new System.Drawing.Size(125, 125);
+            this.L2.TabIndex = 5;
+            this.L2.UseVisualStyleBackColor = true;
+            // 
+            // Z1
+            // 
+            this.Z1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Z1.Location = new System.Drawing.Point(6, 291);
+            this.Z1.Name = "Z1";
+            this.Z1.Size = new System.Drawing.Size(125, 125);
+            this.Z1.TabIndex = 6;
+            this.Z1.UseVisualStyleBackColor = true;
+            // 
+            // Z2
+            // 
+            this.Z2.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Z2.Location = new System.Drawing.Point(137, 291);
+            this.Z2.Name = "Z2";
+            this.Z2.Size = new System.Drawing.Size(125, 125);
+            this.Z2.TabIndex = 7;
+            this.Z2.UseVisualStyleBackColor = true;
+            // 
+            // Z3
+            // 
+            this.Z3.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Z3.Location = new System.Drawing.Point(268, 291);
+            this.Z3.Name = "Z3";
+            this.Z3.Size = new System.Drawing.Size(125, 125);
+            this.Z3.TabIndex = 8;
+            this.Z3.UseVisualStyleBackColor = true;
+            // 
+            // L3
+            // 
+            this.L3.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.L3.Location = new System.Drawing.Point(268, 160);
+            this.L3.Name = "L3";
+            this.L3.Size = new System.Drawing.Size(125, 125);
+            this.L3.TabIndex = 9;
+            this.L3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 426);
+            this.Controls.Add(this.L3);
+            this.Controls.Add(this.Z3);
+            this.Controls.Add(this.Z2);
+            this.Controls.Add(this.Z1);
+            this.Controls.Add(this.L2);
+            this.Controls.Add(this.L1);
+            this.Controls.Add(this.K3);
+            this.Controls.Add(this.K2);
+            this.Controls.Add(this.K1);
+            this.Controls.Add(this.menuStrip1);
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tic Tac Toe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Button K1;
+        private System.Windows.Forms.Button K2;
+        private System.Windows.Forms.Button K3;
+        private System.Windows.Forms.Button L1;
+        private System.Windows.Forms.Button L2;
+        private System.Windows.Forms.Button Z1;
+        private System.Windows.Forms.Button Z2;
+        private System.Windows.Forms.Button Z3;
+        private System.Windows.Forms.Button L3;
+    }
+}
+
